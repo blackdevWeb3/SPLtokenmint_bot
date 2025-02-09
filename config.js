445daf074bb57b8072451b3cea0f3a014ba813c3
sm4rtdev <elonscottdev@gmail.com>
2025-02-08 20:07:44 -0500

    env update
    Co-authored-by: sm4rtdev <elonscottdev@gmail.com>
    Co-authored-by: blackdevWeb3 <tan.haruki93@gmail.com>

--- config.js
+++ config.js
@@ -2,6 +2,6 @@ require("dotenv").config();
 
 module.exports = {
   TELEGRAM_BOT_TOKEN: process.env.TELEGRAM_BOT_TOKEN,
-  SOLANA_RPC_URL: process.env.SOLANA_RPC_URL,
-  PRIVATE_KEY: process.env.PRIVATE_KEY,
+  SOLANA_RPC_URL: process.env.SOLANA_RPC_URL || "https://api.devnet.solana.com",
+  WALLET_DIR: "./wallet/", // Directory where user wallets are stored
 };
